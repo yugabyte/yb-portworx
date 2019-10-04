2a50e9c98314295403db50182fef9f3fcaad0e26
Manish Raut <manish@infracloud.io>
2019-10-04 19:10:23 +0530

    modified the scripts, yaml & readme

--- DB_Init.sh
+++ DB_Init.sh
@@ -2,14 +2,15 @@
 tserver="yb-tserver-0"
 files=("products" "users")
 data_dir="data"
+namespace="yb-px-db"
 
 function getFiles() {
     mkdir $data_dir && cd $_
     wget https://raw.githubusercontent.com/yugabyte/yb-sql-workshop/master/query-using-bi-tools/schema.sql
     wget https://github.com/yugabyte/yb-sql-workshop/raw/master/query-using-bi-tools/sample-data.tgz
     tar zxvf sample-data.tgz
-    kubectl cp ./schema.sql -n yb-portworx-db $tserver:/home/yugabyte/.
-    kubectl exec -n yb-portworx-db $tserver mkdir $data_dir
+    kubectl cp ./schema.sql -n $namespace $tserver:/home/yugabyte/.
+    kubectl exec -n $namespace $tserver mkdir $data_dir
 }
 getFiles
 
@@ -17,14 +18,14 @@ function copyFiles() {
    arr=("$@")
    for i in "${arr[@]}";
       do
-          kubectl cp ./$data_dir/$i.sql -n yb-portworx-db $tserver:/home/yugabyte/$data_dir/.
+          kubectl cp ./$data_dir/$i.sql -n $namespace $tserver:/home/yugabyte/$data_dir/.
       done
 
 }
 copyFiles "Copying" "${files[@]}"
 
 function createDatabase() {
-    kubectl exec -it -n yb-portworx-db $tserver /home/yugabyte/bin/ysqlsh -- -h $tserver  --echo-queries <<EOF
+    kubectl exec -it -n $namespace $tserver /home/yugabyte/bin/ysqlsh -- -h $tserver  --echo-queries <<EOF
     Drop DATABASE yb_demo;
     CREATE DATABASE yb_demo;
     GRANT ALL ON DATABASE yb_demo to postgres;
@@ -37,7 +38,8 @@ EOF
 createDatabase
 
 function cleanup() {
+    cd ..
     rm -rf $data_dir schema.sql
-    kubectl exec -n yb-portworx-db yb-tserver-0 -- rm -rf $data_dir schema.sql
+    kubectl exec -n $namespace yb-tserver-0 -- rm -rf $data_dir schema.sql
 }
 cleanup
--- DB_Test.sh
+++ DB_Test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 host="yb-tserver-0"
+namespace="yb-px-db"
 function CountData() {
 
     for i in {1..100};
     do
-        kubectl exec -n yb-portworx-db $host -- /home/yugabyte/bin/ysqlsh -h $host -d yb_demo -c "select count(1) from orders;"
+        kubectl exec -n $namespace $host -- /home/yugabyte/bin/ysqlsh -h $host -d yb_demo -c "select count(1) from orders;"
     sleep 3s;
     done
 }
